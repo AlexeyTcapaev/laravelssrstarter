9a79e581b663eb13bd53448487d2bf274e854445
AlexeyTcapaev <lexx1995@mail.ru>
2018-06-13 17:07:38 +0300

    init

---
+++ resources/assets/js/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './components/App.vue';
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+
+
+export default new Vue({
+    render: h => h(App)
+});
